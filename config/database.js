30b7dc38bea917209bbbf83113fec11d2ed05953
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-15 05:44:41 +0700

    first bisasss

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ import { Sequelize } from 'sequelize';
 const db = new Sequelize('dbs_pelma', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
+  dialectModule: require('mysql2'),
 });
 
 export default db;
