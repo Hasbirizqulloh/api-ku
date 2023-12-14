40a1eeed1315a815224cc507a3ec2e195e081c1c
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-15 06:20:52 +0700

    berhasil

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2';
 
 const db = new Sequelize('dbs_pelma', 'faikar', 'dfasofd08', {
-  host: 'faikarmoht.my.id',
+  host: 'faikarmocht.my.id',
   dialect: 'mysql',
   dialectModule: mysql2,
 });
