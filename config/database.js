6e16600ee5cd157300e80c689827fdac034ff50e
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-15 06:43:02 +0700

    berhasil

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ import mysql2 from 'mysql2';
 //   dialectModule: mysql2,
 // });
 
-const db = new Sequalize('mysql://faikar:Gaadaygtau100%23@149.28.139.38:3306/dbs_pelma?connection_limit=10&connectiontimeout=30000', {
+const db = new Sequelize('mysql://faikar:Gaadaygtau100%23@149.28.139.38:3306/dbs_pelma?connection_limit=10&connectiontimeout=30000', {
   dialectModule: mysql2,
 });
 
